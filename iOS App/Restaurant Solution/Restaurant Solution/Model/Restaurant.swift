961479afb8a0dc0dd9b1bb7ed81d9a70a10aa8a6
Danny Harding <dannyharding10@gmail.com>
2017-02-04 12:36:44 -0700

    Some more test/beginning stuff for the app, including getting data from server

---
+++ iOS App/Restaurant Solution/Restaurant Solution/Model/Restaurant.swift
@@ -0,0 +1,21 @@
+//
+//  Restaurant.swift
+//  Restaurant Solution
+//
+//  Created by Danny Harding on 1/29/17.
+//  Copyright © 2017 Danny Harding. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+
+	init(name: String, menu: String) {
+		// implement initializer
+	}
+
+	init(json: [String: Any]) {
+		// implement initializer
+	}
+
+}
--- iOS App/Restaurant Solution/Restaurant Solution/ViewController.swift
+++ iOS App/Restaurant Solution/Restaurant Solution/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 	@IBOutlet weak var genericLabel: UILabel!
+	@IBOutlet weak var genericText: UITextView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		getData()
+
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -29,5 +31,73 @@ class ViewController: UIViewController {
 		}
 	}
 
+	@IBAction func clickSecondButton(_ sender: UIButton) {
+		if genericLabel.text == "My Very First Label" {
+			genericLabel.text = "I changed the label!"
+		} else {
+			genericLabel.text = "My Very First Label"
+		}
+	}
+
+	func getData() {
+		print("getting data")
+		let url = URL(string: "http://yodipity.com/api/restaurants/1")
+
+		let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+			DispatchQueue.main.async(execute: {
+				guard error == nil else {
+					print(error!)
+					return
+				}
+				guard let data = data else {
+					print("Data is empty")
+					return
+				}
+
+				let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
+
+				let restaurants = json as! [String : Any]
+
+				// possibly a better way to handle the try?
+				//			do {
+				//				if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any] {
+				//					//Implement your logic
+				//					print(json)
+				//				}
+				//			} catch {
+				//				print("error in JSONSerialization")
+				//			}
+
+				print(json)
+
+
+				var serverResponse = "The following restaurant(s) was(were) received from the server:\n\n{\n"
+				restaurants.keys.forEach { restaurantName in
+					let restaurant = restaurants[restaurantName] as! [String: Any]
+					restaurant.keys.forEach { key in
+						print("here")
+						var stringValue: String;
+						if let string = restaurant[key] as? String {
+							stringValue = string
+						} else if let intValue = restaurant[key] as? Int {
+							stringValue = String(intValue)
+						} else {
+							stringValue = "neither type"
+						}
+						serverResponse += "\t" + key + ": " + stringValue + "\n"
+						print(key)
+						print(restaurant[key])
+					}
+				}
+				serverResponse += "}"
+				self.genericText.text = serverResponse
+			})
+		}
+		
+		print("END")
+		task.resume()
+		
+	}
+	
 }
 
