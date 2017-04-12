cff8ccdf958448fadb9ca76df8c53e57ca552e42
Danny Harding <dannyharding10@gmail.com>
2017-04-12 00:04:44 -0600

    Add visits and ability to checkin

--- iOS App/Restaurant Solution/Restaurant Solution/Visit.swift
+++ iOS App/Restaurant Solution/Restaurant Solution/Visit.swift
@@ -11,9 +11,24 @@ import Foundation
 class Visit {
 	let userID: Int
 	let restaurantID: Int
+	var visitID: Int?
 
 	init(userID: Int, restaurantID: Int) {
 		self.userID = userID
 		self.restaurantID = restaurantID
+
+		setID()
+	}
+
+	func setID() {
+		ServerCommunicator.POST(options: ["route": "/visits", "data": ["user_id": userID, "restaurant_id": restaurantID], "expect": "json"]) {data in
+			guard let data = data as? [String: Any], let visit = data["visit"] as? [String: Any] else {
+				//didn't get the visit right, remove visit from user?
+				print("failed visit")
+				return
+			}
+
+			self.visitID = visit["id"] as! Int
+		}
 	}
 }
